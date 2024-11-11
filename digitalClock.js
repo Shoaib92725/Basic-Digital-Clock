4d025c15cf30e97b6e00d955fd31eaf461cedfe0
shoaib <shoaib1@gmail.com>
2024-11-11 15:04:36 +0530

    New Updates Added

--- digitalClock.js
+++ digitalClock.js
@@ -1,16 +1,35 @@
 const clock = document.querySelector(".clock");
+const button = document.querySelector(".button");
+
+let convert12Hour = true;
 
 const tick = () => {
+  // const times = new Date();
   const timeProvider = new Date();
-  const hour = timeProvider.getHours();
-  const min = timeProvider.getMinutes();
-  const sec = timeProvider.getSeconds();
+
+  let hour = timeProvider.getHours();
+  let min = timeProvider.getMinutes();
+  let sec = timeProvider.getSeconds();
+
+  if (convert12Hour && hour > 12) {
+    hour = hour - 12;
+  }
+
+  // Add leading zeros if needed (for single digit hours, minutes, and seconds)
+  hour = hour < 10 ? `0${hour}` : hour;
+  min = min < 10 ? `0${min}` : min;
+  sec = sec < 10 ? `0${sec}` : sec;
 
   const html = `<span>${hour}</span> :
-    <span>${min}</span> :
-    <span>${sec}</span>`;
+  <span>${min}</span> :
+  <span>${sec}</span>`;
 
   clock.innerHTML = html;
 };
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  convert12Hour = !convert12Hour; // Toggle between 24-hour and 12-hour format
+});
 
 setInterval(tick, 1000);
